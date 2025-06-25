9202da2c38526c2ce1ec9043467ce3d6f88c2648
Rafal Pacler <rafal.pacler@grinn-global.com>
2025-06-25 08:34:50 +0200

    wip

---
+++ lua/custom/plugins/dap.lua
@@ -0,0 +1,40 @@
+return {
+  -- 'mfussenegger/nvim-dap',
+  -- config = function()
+  --   local dap, dapui = require 'dap', require 'dapui'
+  --   dap.listeners.before.attach.dapui_config = function()
+  --     dapui.open()
+  --   end
+  --   dap.listeners.before.launch.dapui_config = function()
+  --     dapui.open()
+  --   end
+  --   dap.listeners.before.event_terminated.dapui_config = function()
+  --     dapui.close()
+  --   end
+  --   dap.listeners.before.event_exited.dapui_config = function()
+  --     dapui.close()
+  --   end
+  --   require('dap').adapters.lldb = {
+  --     type = 'server',
+  --     host = '127.0.0.1',
+  --     port = 13000,
+  --     executable = {
+  --       command = vim.fn.stdpath 'data' .. '/mason/packages/codelldb/extension/adapter/codelldb',
+  --       args = { '--port', '13000' },
+  --
+  --       -- on windows you may have to uncomment this:
+  --       -- detached = false,
+  --     },
+  --   }
+  --   local map = vim.keymap.set
+  --
+  --   map('n', '<Leader>dl', "<cmd>lua require'dap'.step_into()<CR>", { desc = 'Debugger step into' })
+  --   map('n', '<Leader>dj', "<cmd>lua require'dap'.step_over()<CR>", { desc = 'Debugger step over' })
+  --   map('n', '<Leader>dk', "<cmd>lua require'dap'.step_out()<CR>", { desc = 'Debugger step out' })
+  --   map('n', '<Leader>dc', "<cmd>lua require'dap'.continue()<CR>", { desc = 'Debugger continue' })
+  --   map('n', '<Leader>db', "<cmd>lua require'dap'.toggle_breakpoint()<CR>", { desc = 'Debugger toggle breakpoint' })
+  --   map('n', '<Leader>dd', "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", { desc = 'Debugger set conditional breakpoint' })
+  --   map('n', '<Leader>de', "<cmd>lua require'dap'.terminate()<CR>", { desc = 'Debugger reset' })
+  --   map('n', '<Leader>dr', "<cmd>lua require'dap'.run_last()<CR>", { desc = 'Debugger run last' })
+  -- end,
+}
